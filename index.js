cb2f8d3530f69e9a89d1fac49b4dcb9146e32517
davit <dabashidze@seu.edu.ge>
2022-11-05 15:50:18 +0400

    finished the assignment

--- index.js
+++ index.js
@@ -47,5 +47,39 @@
 //     if (typeof array[index] === typeof "") {
 //         console.log(array[index]);
 //     }
-    
+
 // }
+// davaleba 6
+// let array = [
+//   [2, -3, 5, 10],
+//   [25, -24, -11, 100],
+//   [-6, -7, 10],
+// ];
+
+// for (let outer = 0; outer < array.length; outer += 1) {
+//   for (let index = 0; index < array[outer].length; index += 1) {
+//     if (array[outer][index] > 0) {
+//       console.log(array[outer][index]);
+//     }
+//   }
+// }
+// davaleba 7
+// let array = [2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10];
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index] % 2 > 0) {
+//     console.log("odd", array[index]);
+//   } else {
+//     console.log("even", array[index]);
+//   }
+// }
+// davaleba 8
+// let users = [
+//   { username: "giorgi", status: false },
+//   { username: "levani", status: false },
+//   { username: "anna", status: true },
+// ];
+//  for (let index = 0; index < users.length; index +=1) {
+//     if (users[index].status === true){
+//         console.log(users[index]);
+//     }
+//  }
